d02549d5cd724e4a76ca528d18c7d0d85a96eb45
valendrenitos <smaniotto.valentin98@gmail.com>
2024-10-16 10:20:44 +0200

    update To master

---
+++ AMAPG4/Models/Order/IOrderLineDal.cs
@@ -0,0 +1,6 @@
+﻿namespace AMAPG4.Models.Order
+{
+    public class IOrderLineDal
+    {
+    }
+}
---
+++ AMAPG4/Models/Order/OrderLine.cs
@@ -0,0 +1,13 @@
+﻿using AMAPG4.Models.Catalog;
+using XAct;
+
+namespace AMAPG4.Models.Order
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public  int Quantity { get; set; }
+        public float PriceTotal { get; set; } 
+    }
+}
