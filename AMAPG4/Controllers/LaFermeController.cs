4070cf63d22a6d06adebea71ed67bfa21d213672
Francisca Ilabaca <fr23ip@gmail.com>
2024-10-20 19:43:46 +0200

    created the page La Ferme to link to the different types of products

---
+++ AMAPG4/Controllers/LaFermeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AMAPG4.Controllers
+{
+    public class LaFermeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
