147a43e6d2d5e5e1e1a682929b94d2256fbe4857
JohannGourmelon <johann.gourmelon@gmail.com>
2024-10-27 23:08:29 +0100

    updateAccount

--- AMAPG4/Controllers/AccountController.cs
+++ AMAPG4/Controllers/AccountController.cs
@@ -1,40 +1,176 @@
-﻿using AMAPG4.Models.User;
-using Microsoft.AspNetCore.Mvc;
+﻿using AMAPG4.Models;
+using AMAPG4.Models.User;
 using AMAPG4.ViewModels;
-
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace AMAPG4.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        private MyDBContext _bddContext;
+
+        public AccountController()
+        {
+            _bddContext = new MyDBContext();
+        }
+
         public IActionResult Index()
         {
-            UserAccountViewModel UserAccountViewModel =
+            UserAccountViewModel userAccountViewModel =
                 new UserAccountViewModel() { Authentication = HttpContext.User.Identity.IsAuthenticated };
-            if (UserAccountViewModel.Authentication)
+
+            if (userAccountViewModel.Authentication)
             {
                 using (UserAccountDal userAccountDal = new UserAccountDal())
                 {
-                    UserAccountViewModel.UserAccount = userAccountDal.GetUserAccount(HttpContext.User.Identity.Name);
+                    userAccountViewModel.UserAccount = userAccountDal.GetUserAccount(HttpContext.User.Identity.Name);
                 }
                 using (IndividualDal individualDal = new IndividualDal())
                 {
-                    UserAccountViewModel.Individual = individualDal.GetIndividualByUserAccount(UserAccountViewModel.UserAccount.Id);
+                    userAccountViewModel.Individual = individualDal.GetIndividualByUserAccount(userAccountViewModel.UserAccount.Id);
                 }
                 using (ProducerDal producerDal = new ProducerDal())
                 {
-                    UserAccountViewModel.Producer = producerDal.GetProducerByUserAccount(UserAccountViewModel.UserAccount.Id);
+                    userAccountViewModel.Producer = producerDal.GetProducerByUserAccount(userAccountViewModel.UserAccount.Id);
                 }
                 using (CEDal ceDal = new CEDal())
                 {
-                    UserAccountViewModel.CE = ceDal.GetCEByUserAccount(UserAccountViewModel.UserAccount.Id);
+                    userAccountViewModel.CE = ceDal.GetCEByUserAccount(userAccountViewModel.UserAccount.Id);
                 }
-                return View(UserAccountViewModel);
             }
-            return View(UserAccountViewModel);
+
+            return View(userAccountViewModel);
         }
+        [HttpGet]
+        public IActionResult UpdateI()
+        {
+            UserAccountViewModel model = new UserAccountViewModel()
+            {
+                Authentication = HttpContext.User.Identity.IsAuthenticated
+            };
+            Console.WriteLine(model.Authentication);
+            if (model.Authentication)
+            {
+               
+                using (UserAccountDal userAccountDal = new UserAccountDal())
+                {
+                    model.UserAccount = userAccountDal.GetUserAccount(HttpContext.User.Identity.Name);
+                    Console.WriteLine(model.UserAccount.Id);
+                }
+                using (IndividualDal individualDal = new IndividualDal())
+                {
+                    model.Individual = individualDal.GetIndividualByUserAccount(model.UserAccount.Id);
+                    Console.WriteLine(model.Individual.Id);
+                }
+            }
+
+            return View("Update", model);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateI(UserAccountViewModel model)
+        {
+                      
+                using (IndividualDal individualDal = new IndividualDal())
+                {
+                    Individual individual = individualDal.GetIndividualByUserAccount(model.UserAccount.Id);
+                    individual.Account.Name = model.UserAccount.Name;
+                    individual.FirstName = model.Individual.FirstName;
+                    individual.Account.Email = model.UserAccount.Email;
+                    individual.Account.Address = model.UserAccount.Address;
+                    individual.Account.Phone = model.UserAccount.Phone;
+                    individual.IsVolunteer = model.Individual.IsVolunteer;
+                    individualDal.UpdateIndividual(individual);
+                }
+        
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateP()
+        {
+            UserAccountViewModel model = new UserAccountViewModel()
+            {
+                Authentication = HttpContext.User.Identity.IsAuthenticated
+            };
+
+            if (model.Authentication)
+            {
+                using (UserAccountDal userAccountDal = new UserAccountDal())
+                {
+                    model.UserAccount = userAccountDal.GetUserAccount(HttpContext.User.Identity.Name);
+                }
+                using (ProducerDal producerDal = new ProducerDal())
+                {
+                    model.Producer = producerDal.GetProducerByUserAccount(model.UserAccount.Id);
+                }
+            }
+
+            return View("Update", model);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateP(UserAccountViewModel model)
+        {
+                using (ProducerDal producerDal = new ProducerDal())
+                {
+                    Producer producer = producerDal.GetProducerByUserAccount(model.UserAccount.Id);
+                    producer.Account.Name = model.UserAccount.Name;
+                    producer.Account.Email = model.UserAccount.Email;
+                    producer.Account.Address = model.UserAccount.Address;
+                    producer.Account.Phone = model.UserAccount.Phone;
+                    producer.ContactName = model.Producer.ContactName;
+                    producer.Siret = model.Producer.Siret;
+                    producer.RIB = model.Producer.RIB;
+                    producerDal.UpdateProducer(producer);
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCE()
+        {
+            UserAccountViewModel model = new UserAccountViewModel()
+            {
+                Authentication = HttpContext.User.Identity.IsAuthenticated
+            };
+
+            if (model.Authentication)
+            {
+                using (UserAccountDal userAccountDal = new UserAccountDal())
+                {
+                    model.UserAccount = userAccountDal.GetUserAccount(HttpContext.User.Identity.Name);
+                }
+                using (CEDal ceDal = new CEDal())
+                {
+                    model.CE = ceDal.GetCEByUserAccount(model.UserAccount.Id);
+                }
+            }
+
+            return View("Update", model);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCE(UserAccountViewModel model)
+        {            
+                using (CEDal ceDal = new CEDal())
+                {
+                    CE ce = ceDal.GetCEByUserAccount(model.UserAccount.Id);
+                    ce.Account.Name = model.UserAccount.Name;
+                    ce.Account.Email = model.UserAccount.Email;
+                    ce.Account.Address = model.UserAccount.Address;
+                    ce.Account.Phone = model.UserAccount.Phone;
+                    ce.ContactName = model.CE.ContactName;
+                    ce.NumberOfEmployees = model.CE.NumberOfEmployees;
+                    ceDal.UpdateCE(ce);
+            }
+            
+            return RedirectToAction("Index");
+        }
+
     }
 }
